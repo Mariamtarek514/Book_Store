c068017897cdb673a2b60984a31bb71d954a822e
Mariam_Tarek <mariamelsarag44@gmail.com>
2023-09-29 14:01:27 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import AppLayout from "./Pages/AppLayout";
@@ -9,16 +9,14 @@ import BookDetails from "./Components/BookDetails";
 const App = () => {
     return (
         <div className="App ">
-            <Router>
-                <Routes>
-                    <Route path="/" element={<AppLayout />}>
-                        <Route path="home" element={<Home />} />
-                        <Route path="about" element={<About />} />
-                        <Route path="book" element={<BookList />} />
-                        <Route path="book/:id" element={<BookDetails />} />
-                    </Route>
-                </Routes>
-            </Router>
+            <Routes>
+                <Route path="/" element={<AppLayout />}>
+                    <Route path="home" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="book" element={<BookList />} />
+                    <Route path="book/:id" element={<BookDetails />} />
+                </Route>
+            </Routes>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from "./App";
 import "../src/Assets/Styles/main/main.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
